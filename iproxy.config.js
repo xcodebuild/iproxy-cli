317ce603105cbcca742da2a9748ac294e58b29ef
shejinxin <shejinxin@corp.netease.com>
2021-11-16 17:35:33 +0800

    inital commit

---
+++ iproxy.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'test',
+  id: 'iproxy-test-rule',
+  rule: '# no content'
+}
--- src/entry/index.ts
+++ src/entry/index.ts
@@ -1,3 +1,66 @@
-export function test() {
-	return 3;
+import fs from 'fs';
+import colors from 'colors';
+import shell from 'shelljs';
+import path from 'path';
+import { exec } from 'child_process';
+
+const isRunning = (query: string) => {
+    let platform = process.platform;
+    let cmd = '';
+    switch (platform) {
+        case 'win32' : cmd = `tasklist`; break;
+        case 'darwin' : cmd = `ps -ax | grep ${query}`; break;
+        case 'linux' : cmd = `ps -A`; break;
+        default: break;
+    }
+	return new Promise(resolve => {
+		exec(cmd, (err, stdout, stderr) => {
+			resolve(stdout.toLowerCase().indexOf(query.toLowerCase()) > -1);
+		});
+	});
 }
+
+const run = async () => {
+
+	const IPROXY_BIN = '/Applications/iProxy.app/Contents/MacOS/iProxy';
+	const DOC_URL = 'https://github.com/xcodebuild/iproxy';
+
+	if (!fs.existsSync(IPROXY_BIN)) {
+		console.error(colors.red(`iProxy is not installed! ${DOC_URL}`));
+		process.exit(1);
+	}
+
+	if (!await isRunning('iProxy')) {
+		console.error(colors.red(`iProxy is not running! ${DOC_URL}`));
+		process.exit(1);
+	}
+	
+	const CONFIG_FILE = path.join(process.cwd(), 'iproxy.config.js');
+	
+	if (!fs.existsSync(CONFIG_FILE)) {
+		console.error(colors.red(`iproxy.config.js is not found! ${DOC_URL}`));
+		process.exit(1);
+	}
+	
+	const config = require(CONFIG_FILE) as {
+		name: string;
+		id: string;
+		rule: string;
+	};
+	
+	const command = {
+		type: 'active-rule',
+		rule: config,
+	};
+	
+	const output = shell.exec(`${IPROXY_BIN} --call ${decodeURIComponent(JSON.stringify(command))}`).stdout;
+	
+	if (!output.includes('[call-success]')) {
+		console.error(colors.red(`Your iProxy is out of date. Please update iProxy to latest version: ${DOC_URL}`));
+		process.exit(2);
+	}
+	
+	console.log(colors.green(`iProxy rule is actived! ${config.name}`));
+};
+
+run();
